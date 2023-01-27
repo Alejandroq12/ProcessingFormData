24ff99dd2416fa2218faac438ec63ed9548856f1
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-01-27 02:02:44 -0600

    feat: a new feature
    Adding a form and preventing in asecure way to accept only secure inputs by using htmlspecialchars and ENT_QUOTES

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-
+  if(isset($_POST['searchterm'])) {
+    echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
+  }
 ?>
+
+<form
+    action=""
+    method="post">
+    <input type="text" name="searchterm">
+    <input type="submit" value="Search">
+</form>
